c06655048306f4295fa95a8cf976dcedd77a01bd
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-05-21 11:02:42 -0700

    update for container registry

--- DEPLOY.sh
+++ DEPLOY.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION="gcr.io/atsgo-340504/ats-images-httpd:0.0.4";
+VERSION="us-central1-docker.pkg.dev/atsgo-340504/ats-images/ats-images-httpd:0.0.5";
 
 docker build -t $VERSION .;
 docker push $VERSION
